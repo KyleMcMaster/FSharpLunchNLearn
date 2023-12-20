adebbcf47a3e8c03c5dea8b6e05cb20d55776920
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2023-12-20 17:13:16 -0500

    Add Contributor domain module

---
+++ src/Domain.fs
@@ -0,0 +1,25 @@
+namespace FSharpWebApp.Domain
+
+module Domain =
+    type ContributorStatus =
+        | Community
+        | Company
+        | NotSet
+
+    type Contributor =
+        { id: int
+          name: string
+          status: ContributorStatus }
+
+    let contributors =
+        [ { id = 1
+            name = "Kyle McMaster"
+            status = Company }
+          { id = 2
+            name = "John Doe"
+            status = Community } ]
+
+    let getContributors () = contributors
+
+    let getContributorById id =
+        contributors |> List.tryFind (fun c -> c.id = id)
