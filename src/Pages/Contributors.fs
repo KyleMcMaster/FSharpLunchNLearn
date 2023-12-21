3b518ec94608e6e02a5bd6a016b9a70068adfd87
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2023-12-20 20:41:59 -0500

    Contributor list view working

--- src/Pages/Contributors.fs
+++ src/Pages/Contributors.fs
@@ -3,6 +3,19 @@ namespace FSharpWebApp.Pages.ContributorsPage
 module ContributorsPage =
     open Falco
     open Falco.Markup
+    open FSharpWebApp.Domain.Domain
+
+    let contributorListHtml contributors =
+        contributors
+        |> List.map (fun c ->
+            Elem.div
+                []
+                [ Elem.h2 [] [ Text.raw c.name ]
+                  Elem.p [] [ Text.raw (sprintf "%A" c.id) ]
+                  Elem.p [] [ Text.raw (sprintf "%A" c.status) ] ])
+
+    let contributorHtml =
+        [ Elem.h1 [] [ Text.raw "Contributors" ] ] @ contributorListHtml contributors
 
     let handleHtml: HttpHandler =
-        Response.ofHtml (Elem.html [] [ Elem.body [] [ Elem.main [] [ Elem.h1 [] [ Text.raw "Contributors" ] ] ] ])
+        Response.ofHtml (Elem.html [] [ Elem.body [] [ Elem.main [] contributorHtml ] ])
