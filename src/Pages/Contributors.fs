4833e153cd1cef476d1312ab25855732364093e5
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2024-01-06 20:01:25 -0500

    small tweaks

--- src/Pages/Contributors.fs
+++ src/Pages/Contributors.fs
@@ -20,6 +20,6 @@ module ContributorsPage =
           Elem.div [] (contributors |> List.map contributorHtml) ]
 
     // TODO - this is a bit of a hack, but it works for now
-    let contributorPage = (getContributors >> contributorsListHtml) ()
+    let contributorPage: XmlNode list = (getContributors >> contributorsListHtml) ()
 
     let handleHtml: HttpHandler = Response.ofHtml (Layout.layout title contributorPage)
--- src/Pages/Index.fs
+++ src/Pages/Index.fs
@@ -7,12 +7,17 @@ module IndexPage =
     open Falco.Markup.Text
 
     let appTitle = "Lunch-n-Learn App"
-    let heading = "Index"
+    let pageTitle = "Index"
 
     let html =
         html
             [ lang "en" ]
             [ head [] [ title [] [ raw appTitle ] ]
-              body [] [ main [] [ h1 [] [ raw heading ]; p [] [ rawf "%s %s" "Kyle" "McMaster" ] ] ] ]
+              body
+                  []
+                  [ main
+                        []
+                        [ h1 [] [ raw pageTitle ]
+                          p [] [ rawf "Hello my name is %s %s" "Kyle" "McMaster" ] ] ] ]
 
     let handleHtml: HttpHandler = Response.ofHtml html
